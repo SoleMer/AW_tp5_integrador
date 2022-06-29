39a007e825e4edb340c0860da98c8780b60fb669
solemer <soledadmerino.1994@gmail.com>
2022-06-28 21:38:26 -0300

    iniciado swagger

---
+++ src/swagger-test/AwTp5SwaggerTest.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwTp5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwTp5Application.class, args);
+    }
+
+}
--- src/test/java/com/aw/awtp5/AwTp5ApplicationTests.java
+++ src/test/java/com/aw/awtp5/AwTp5ApplicationTests.java
@@ -2,12 +2,14 @@ package com.aw.awtp5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootTest
 class AwTp5ApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(String[] args) {
+        SpringApplication.run(AwTp5Application.class, args);
     }
 
 }
